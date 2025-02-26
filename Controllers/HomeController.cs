149348d340136add654888171bcb038858ac7644
uboston <uboston@byu.edu>
2025-02-26 10:06:12 -0700

    Home Controller 1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,9 +4,15 @@ using Mission08_Team0111.Models;
 
 namespace Mission08_Team0111.Controllers;
 
+// Main controller
 public class HomeController : Controller
 {
+        private TaskContext _context;
     
+        public HomeController (TaskContext temp)
+        {
+            _context = temp;
+        }
     public IActionResult Index()
     {
         return View();
